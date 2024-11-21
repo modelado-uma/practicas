9e87f05f2a1a5a4a547160079467283045579501
David Villalba Fernández <163408624+DavidVillalba1@users.noreply.github.com>
2024-11-21 10:01:10 +0100

    pmbok

---
+++ practica3/Practica3Modelado/src/ejercicio1/Donante.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+import java.util.Date;
+
+public class Donante extends Socio{
+
+	public Donante(Date registro) {
+		super(registro);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ practica3/Practica3Modelado/src/ejercicio1/Socio.java
@@ -0,0 +1,14 @@
+package ejercicio1;
+import java.util.*;
+
+public class Socio {
+	private final Date registro;
+	
+	public Socio (Date registro) {
+		this.registro = registro;
+	}
+	
+	public Date getRegistro() {
+		return this.registro;
+	}
+}
